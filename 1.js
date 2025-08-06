592965b71373c8e09501645de5ef647003da4bdd
Krish-B82 <bathanikrish00@gmail.com>
2025-08-07 00:36:23 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,4 +2,4 @@ console.log("Hello, developer");
 const accountID = "12345";
 let accountEmail = "krishpatel080205@gmail.com"
 var accountPassword = "12345678";
-accountCity = "Ahmedabad";
+accountCity = "Anand";
